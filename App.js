702d0b53333348103958872750b204023ddbb7ad
Xenosmurf <100067788+Xenosmurf@users.noreply.github.com>
2023-06-13 17:59:31 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'design')));
+
+// Define a route to serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'design', 'index.html'));
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'design', 'register.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
